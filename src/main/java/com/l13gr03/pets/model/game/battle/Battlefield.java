f4d305add936e94f5732b750670362f2cdf7e1e3
Rodrigo de Sousa <up202205751@up.pt>
2023-11-26 15:57:17 +0000

    changed Battlefield

--- src/main/java/com/l13gr03/pets/model/game/battle/Battlefield.java
+++ src/main/java/com/l13gr03/pets/model/game/battle/Battlefield.java
@@ -1,8 +1,14 @@
 package com.l13gr03.pets.model.game.battle;
 
+import com.l13gr03.pets.model.Position;
 import com.l13gr03.pets.model.game.entities.Entity;
 
 public class Battlefield {
-    private Entity p1, p2;
+    private int height, width;
+    private Entity.Attack attack;
+    private int hp1, hp2;
+    private Position position1, position2;
+    private Entity[] party1;
+    private Entity[] party2;
 
 }
