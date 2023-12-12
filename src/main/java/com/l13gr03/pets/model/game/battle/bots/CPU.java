05682c6c4c0a68cef9654fdf3d14bb38d875d762
Rodrigo de Sousa <up202205751@up.pt>
2023-12-12 15:45:57 +0000

    Created names for the cpu party entities

--- src/main/java/com/l13gr03/pets/model/game/battle/bots/CPU.java
+++ src/main/java/com/l13gr03/pets/model/game/battle/bots/CPU.java
@@ -33,70 +33,70 @@ public abstract class CPU  {
         int[] a = r.generate3();
         Entity p1,p2,p3;
         if (a[0] == 1) {
-             p1 = new AquaticAlly("");
+             p1 = new AquaticAlly("Blastem");
         }
         else if (a[0] == 2) {
-             p1 = new BiomorphicBeast("");
+             p1 = new BiomorphicBeast("Angree");
         }
         else if (a[0] == 3) {
-             p1 = new CelestialGuardian("");
+             p1 = new CelestialGuardian("Doxxis");
         }
         else if (a[0] == 4) {
-             p1 = new CyberneticCompanion("");
+             p1 = new CyberneticCompanion("Metagrosso");
         }
         else if (a[0] == 5) {
-             p1 = new LavaLurker("");
+             p1 = new LavaLurker("Nekizal");
         }
         else if (a[0] == 6) {
-             p1 = new ShadowSpecter("");
+             p1 = new ShadowSpecter("Nocturnyx");
         }
         else {
-             p1 = new StoneGolem("");
+             p1 = new StoneGolem("Big7");
         }
 
         int j = a[1];
         if (j == 1) {
-            p2 = new AquaticAlly("");
+            p2 = new AquaticAlly("Blastem");
         }
         else if (j == 2) {
-             p2 = new BiomorphicBeast("");
+             p2 = new BiomorphicBeast("Angree");
         }
         else if (j == 3) {
-             p2 = new CelestialGuardian("");
+             p2 = new CelestialGuardian("Doxxis");
         }
         else if (j == 4) {
-             p2 = new CyberneticCompanion("");
+             p2 = new CyberneticCompanion("Metagrosso");
         }
         else if (j == 5) {
-             p2 = new LavaLurker("");
+             p2 = new LavaLurker("Nezikal");
         }
         else if (j == 6) {
-             p2 = new ShadowSpecter("");
+             p2 = new ShadowSpecter("Nocturnyx");
         }
         else {
-             p2 = new StoneGolem("");
+             p2 = new StoneGolem("Big7");
         }
         int d = a[2];
         if (d == 1) {
-             p3 = new AquaticAlly("");
+             p3 = new AquaticAlly("Blastem");
         }
         else if (d == 2) {
-             p3 = new BiomorphicBeast("");
+             p3 = new BiomorphicBeast("Angree");
         }
         else if (d == 3) {
-             p3 = new CelestialGuardian("");
+             p3 = new CelestialGuardian("Doxxis");
         }
         else if (d == 4) {
-             p3 = new CyberneticCompanion("");
+             p3 = new CyberneticCompanion("Metagrosso");
         }
         else if (d == 5) {
-             p3 = new LavaLurker("");
+             p3 = new LavaLurker("Nezikal");
         }
         else if (d == 6) {
-             p3 = new ShadowSpecter("");
+             p3 = new ShadowSpecter("Nocturnyx");
         }
         else {
-             p3 = new StoneGolem("");
+             p3 = new StoneGolem("Big7");
         }
         Party party = new Party(p1,p2,p3);
         return party;
